814d221dc120270e418656ebbbefe4ad23ec68f3
dlooze85-cloud <dlooze85@gmail.com>
2026-07-12 20:26:41 +0200

    Update exercises.js

--- data/exercises.js
+++ data/exercises.js
@@ -1,57 +1,22 @@
 const exercises = [
-
 {
-id:1,
-name:"Push-ups",
-sets:3,
-reps:"10",
-rest:60,
-muscle:"Borst • Schouders • Triceps"
+    id:1,
+    name:"Push-ups",
+    image:"💪",
+    sets:3,
+    reps:"10",
+    rest:60,
+    muscle:"Borst • Schouders • Triceps",
+    description:"Houd je lichaam recht en laat je borst gecontroleerd zakken."
 },
-
 {
-id:2,
-name:"Squats",
-sets:3,
-reps:"15",
-rest:60,
-muscle:"Benen • Billen"
-},
-
-{
-id:3,
-name:"Glute Bridge",
-sets:3,
-reps:"15",
-rest:45,
-muscle:"Billen • Hamstrings"
-},
-
-{
-id:4,
-name:"Plank",
-sets:3,
-reps:"30 sec",
-rest:45,
-muscle:"Core"
-},
-
-{
-id:5,
-name:"Reverse Lunges",
-sets:3,
-reps:"12",
-rest:60,
-muscle:"Benen"
-},
-
-{
-id:6,
-name:"Mountain Climbers",
-sets:3,
-reps:"20",
-rest:45,
-muscle:"Core • Conditie"
+    id:2,
+    name:"Squats",
+    image:"🦵",
+    sets:3,
+    reps:"15",
+    rest:60,
+    muscle:"Benen • Billen",
+    description:"Zak totdat je bovenbenen ongeveer parallel aan de vloer zijn."
 }
-
 ];
