f2a02cac5fa93268ad2cc9499c01b76906fa0438
dlooze85-cloud <dlooze85@gmail.com>
2026-07-12 19:49:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,87 @@
-const knop = document.getElementById("timerButton");
-const timer = document.getElementById("timer");
+const startButton = document.getElementById("startButton");
 
-knop.addEventListener("click", () => {
+startButton.addEventListener("click", startWorkout);
 
-let seconden = 60;
+function startWorkout() {
 
-timer.innerHTML = seconden + " sec";
+    const hero = document.querySelector(".hero");
 
-const interval = setInterval(function(){
+    hero.innerHTML = `
 
-seconden--;
+        <p class="small">Training gestart</p>
 
-timer.innerHTML = seconden + " sec";
+        <h2>🔥 Full Body Beginner</h2>
 
-if(seconden<=0){
+        <br>
 
-clearInterval(interval);
+        <h3>Oefening 1 / 8</h3>
 
-timer.innerHTML="✅ Volgende oefening!";
+        <h2>Push-ups</h2>
+
+        <p>3 sets × 10 herhalingen</p>
+
+        <br>
+
+        <button id="nextExercise">
+        OEFENING VOLTOOID
+        </button>
+
+    `;
+
+    document
+        .getElementById("nextExercise")
+        .addEventListener("click", nextExercise);
 
 }
 
-},1000);
+function nextExercise() {
 
-});
+    const hero = document.querySelector(".hero");
+
+    hero.innerHTML = `
+
+        <p class="small">Goed bezig 💪</p>
+
+        <h2>Rust</h2>
+
+        <h1 id="timer">60</h1>
+
+        <p>Volgende oefening start zo...</p>
+
+    `;
+
+    let seconden = 60;
+
+    const teller = document.getElementById("timer");
+
+    const interval = setInterval(() => {
+
+        seconden--;
+
+        teller.innerHTML = seconden;
+
+        if (seconden <= 0) {
+
+            clearInterval(interval);
+
+            hero.innerHTML = `
+
+                <p class="small">Oefening 2 / 8</p>
+
+                <h2>Squats</h2>
+
+                <p>3 sets × 15 herhalingen</p>
+
+                <br>
+
+                <button onclick="location.reload()">
+                Terug naar Home
+                </button>
+
+            `;
+
+        }
+
+    }, 1000);
+
+}
