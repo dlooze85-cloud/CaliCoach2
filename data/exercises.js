0d47a37149bf265a3c5c5701c27c1c9bc917d48b
dlooze85-cloud <dlooze85@gmail.com>
2026-07-12 20:18:45 +0200

    Create exercises.js

---
+++ data/exercises.js
@@ -0,0 +1,57 @@
+const exercises = [
+
+{
+id:1,
+name:"Push-ups",
+sets:3,
+reps:"10",
+rest:60,
+muscle:"Borst • Schouders • Triceps"
+},
+
+{
+id:2,
+name:"Squats",
+sets:3,
+reps:"15",
+rest:60,
+muscle:"Benen • Billen"
+},
+
+{
+id:3,
+name:"Glute Bridge",
+sets:3,
+reps:"15",
+rest:45,
+muscle:"Billen • Hamstrings"
+},
+
+{
+id:4,
+name:"Plank",
+sets:3,
+reps:"30 sec",
+rest:45,
+muscle:"Core"
+},
+
+{
+id:5,
+name:"Reverse Lunges",
+sets:3,
+reps:"12",
+rest:60,
+muscle:"Benen"
+},
+
+{
+id:6,
+name:"Mountain Climbers",
+sets:3,
+reps:"20",
+rest:45,
+muscle:"Core • Conditie"
+}
+
+];
