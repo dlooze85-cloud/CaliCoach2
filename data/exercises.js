3dfd339d96cafc89616588b942d7c15a7f503e9b
dlooze85-cloud <dlooze85@gmail.com>
2026-07-13 18:57:55 +0200

    Update exercises.js

--- data/exercises.js
+++ data/exercises.js
@@ -1,27 +1,29 @@
 const exercises = [
+
 {
-    id:1,
-    name:"Push-ups",
-    image:"assets/pushup.svg",
-    sets:3,
-    reps:"10",
-    rest:60,
-    muscle:"Borst • Schouders • Triceps",
-    difficulty:"Beginner",
-    calories:18,
-    description:"Houd je lichaam in een rechte lijn. Laat je borst rustig zakken en duw krachtig omhoog."
+    id: 1,
+    name: "Push-ups",
+    image: "assets/pushup.svg",
+    sets: 3,
+    reps: "10",
+    rest: 60,
+    muscle: "Borst • Schouders • Triceps",
+    difficulty: "Beginner",
+    calories: 18,
+    description: "Houd je lichaam in een rechte lijn. Laat je borst rustig zakken en duw jezelf krachtig omhoog."
 },
+
 {
-    id:2,
-    name:"Squats",
-    image:"🦵",
-    sets:3,
-    reps:"15",
-    rest:60,
-    muscle:"Benen • Billen",
-    difficulty:"Beginner",
-    calories:20,
-    description:"Zak gecontroleerd totdat je bovenbenen ongeveer parallel aan de vloer zijn. Houd je rug recht en duw jezelf krachtig omhoog."
+    id: 2,
+    name: "Squats",
+    image: "🦵",
+    sets: 3,
+    reps: "15",
+    rest: 60,
+    muscle: "Benen • Billen",
+    difficulty: "Beginner",
+    calories: 20,
+    description: "Zak gecontroleerd totdat je bovenbenen ongeveer parallel aan de vloer zijn. Houd je rug recht."
 }
-}
+
 ];
