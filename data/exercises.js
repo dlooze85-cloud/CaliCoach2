39c9cae34817099135a16984385a848ada761634
dlooze85-cloud <dlooze85@gmail.com>
2026-07-13 08:52:39 +0200

    Update exercises.js

--- data/exercises.js
+++ data/exercises.js
@@ -11,7 +11,6 @@ const exercises = [
     calories:18,
     description:"Houd je lichaam in een rechte lijn. Laat je borst rustig zakken en duw krachtig omhoog."
 },
-},
 {
     id:2,
     name:"Squats",
@@ -20,6 +19,9 @@ const exercises = [
     reps:"15",
     rest:60,
     muscle:"Benen • Billen",
-    description:"Zak totdat je bovenbenen ongeveer parallel aan de vloer zijn."
+    difficulty:"Beginner",
+    calories:20,
+    description:"Zak gecontroleerd totdat je bovenbenen ongeveer parallel aan de vloer zijn. Houd je rug recht en duw jezelf krachtig omhoog."
+}
 }
 ];
