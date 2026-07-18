f3ae8c76989543c64f4bdd59bf90d49d9e8be830
dlooze85-cloud <dlooze85@gmail.com>
2026-07-18 14:49:11 +0200

    Update achievements.js

--- js/achievements.js
+++ js/achievements.js
@@ -1,1 +1,52 @@
+const achievements = [
 
+{
+    title: "🥇 Eerste training",
+    goal: 1
+},
+
+{
+    title: "🔥 Op dreef",
+    goal: 10
+},
+
+{
+    title: "💯 Doorzetter",
+    goal: 100
+},
+
+{
+    title: "👑 Fitness Legend",
+    goal: 250
+}
+
+];
+
+function renderAchievements(){
+
+    const trainingen = getTrainingen();
+
+    let html = "<h2>🏆 Achievements</h2>";
+
+    achievements.forEach(a=>{
+
+        const gehaald = trainingen >= a.goal;
+
+        html += `
+        <div class="achievement ${gehaald ? "done" : ""}">
+            <div>
+                <strong>${a.title}</strong><br>
+                ${Math.min(trainingen,a.goal)} / ${a.goal}
+            </div>
+
+            <div>
+                ${gehaald ? "✅" : "🔒"}
+            </div>
+        </div>
+        `;
+
+    });
+
+    document.getElementById("achievementList").innerHTML = html;
+
+}
